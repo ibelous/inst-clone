1227ae45371a2469e7795d44f2c4d63ad9ce787b
white <igrek145@gmail.com>
2019-03-19 11:04:49 +0300

    started 5th task

--- UI/script.js
+++ UI/script.js
@@ -20,18 +20,17 @@ postArray = [
         likers:['User1','User2','User3','User4']
     }
 ];
-console.log('started');
-var modul = (function () {
-    function addPhotoPost(post){
-        if(validatePhotoPost(post)){
-            postArray.push(post);
-            console.log('added post');
-            return true;
-            }
-        console.log('invalid post');
-        return false;
-        }
-    function validatePhotoPost(post) {
+class PostModel {
+    constructor(posts) {
+        this._defaultFilterConfig = {
+            dateFrom: new Date(-8640000000000000),
+            dateTo: new Date(8640000000000000),
+            authorName: '',
+            hashtags: [],
+        };
+        this._photoPosts = posts;
+    }
+    _validatePhotoPost(post) {
         if (!post.hasOwnProperty("id") || typeof post.id != "string" ||
             !post.hasOwnProperty("description") || post.description.length >= 200 || typeof post.description != "string" ||
             !post.hasOwnProperty("createdAt") ||
@@ -46,8 +45,52 @@ var modul = (function () {
             return true;
         }
     }
-    function getPhotoPost(id) {
-        var result;
+    addAll(posts) {
+        const notValid = [];
+        for (let i = 0; i < posts.length; i += 1) {
+            if (this._validatePhotoPost(posts[i]) === true) {
+                this._photoPosts.push(posts[i]);
+                console.log(`Post with id ${posts[i].id} added.`);
+            } else {
+                notValid.push(posts[i]);
+            }
+        }
+        console.log('Not valid posts:');
+        console.log(notValid);
+        return notValid;
+    }
+
+    addPhotoPost(post){
+        if(validatePhotoPost(post)){
+            postArray.push(post);
+            console.log('added post');
+            return true;
+        }
+        console.log('invalid post');
+        return false;
+    }
+    getPhotoPosts(skip = 0, count = 10, filterConfig = this._defaultFilterConfig) {
+        const filtratedPosts = this._photoPosts.filter(
+          post => post.createdAt.getTime() >= filterConfig.dateFrom.getTime()
+            && post.createdAt.getTime() <= filterConfig.dateTo.getTime()
+            && (post.author === filterConfig.authorName
+              || filterConfig.authorName === '')
+        ).sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime());
+        const result = [];
+        let number = 0;
+        for (let i = skip; number < count && i < filtratedPosts.length; i += 1) {
+            result.push(filtratedPosts[i]);
+            number += 1;
+        }
+        console.log(`skip: ${skip}; count: ${count}; filterConfig:`);
+        console.log(filterConfig);
+        console.log('Result:');
+        console.log(result);
+        return result;
+    }
+    getPhotoPost(id) {
+        let  result;
+        postArray.find()
         postArray.forEach(function (item) {
             if(item.id == id){
                 console.log("id " + id + " found ");
@@ -60,7 +103,7 @@ var modul = (function () {
         }
         console.log("id " + id + " is not found.");
     }
-    function removePhotoPost(id) {
+    removePhotoPost(id) {
         var index = -1;
         if(getPhotoPost(id)) {
             index = getPhotoPost(id).id;
@@ -75,5 +118,27 @@ var modul = (function () {
             return false;
         }
     }
-})();
+    editPhotoPost(id, edits) {
+        const post = this.getPhotoPost(id);
+        if (post !== undefined) {
+            const postCopy = Object.assign(post);
+            const fields = Object.keys(edits);
+            for (let i = 0; i < fields.length; i += 1) {
+                if (fields[i] !== 'id' && fields[i] !== 'author' && fields[i] !== 'createdAt' && fields[i] !== 'likes') {
+                    postCopy[fields[i]] = edits[fields[i]];
+                }
+            }
+            this.removePhotoPost(id);
+            if (this._validatePhotoPost(postCopy)) {
+                this.addPhotoPost(postCopy);
+                console.log('Post successfully changed.');
+                return true;
+            }
+            this.addPhotoPost(post);
+        }
+        console.log('Post not changed.');
+        return false;
+    }
+}
+console.log('started');
 
